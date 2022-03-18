fa8a43abd1a65a1460e96f5feebb63930389b6b4
Toni B.Lopez local <tonibassols@outlook.es>
2022-03-18 19:52:52 +0100

    I add async/callback.js folder ( and i used setTimeout() & setInterval()

---
+++ async/callback.js
@@ -0,0 +1,47 @@
+//  Test One:
+
+function soyAsincrona() {
+
+    setTimeout(() => {
+        console.log('Estoy siendo asíncrona');
+    }, 1000);
+}
+
+console.log('iniciando proceso...');
+soyAsincrona()
+console.log('terminando proceso...');
+
+
+//  Test Two:
+
+// A esta asincronía le he pasado el mismo tiempo de intervalo de ejecución pero
+// espero a que se llame una para llamar la otra:
+
+function asyncOne(name, firstCallback) {
+
+    setTimeout(() => { // setTimeout se ejecuta una sola vez cuando llega a la línea de ejecución y se cumple el tiempo de espera hasta su ejecución
+
+        console.log('Primera asincronía '+ name);
+        firstCallback(name)
+    }, 1000);
+}
+
+function asyncTwo(name, secondCallback) {
+
+    setTimeout(() => {
+
+        console.log('Segunda asincronía '+ name);
+        secondCallback()
+    }, 1000);
+}
+
+asyncOne('Toni', (name) => {
+
+    asyncTwo(name, () => {
+
+        setInterval(() => { // setInterval se ejecuta de forma infinita cada X tiempo que le hayamos añadido
+
+            console.log('Soy más compejo aún '+ name);
+        }, 1000);
+    })
+})

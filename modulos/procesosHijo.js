2349b488ba19a9fac1c9b0f80daa3c66228d2079
Toni B.Lopez local <tonibassols@outlook.es>
2022-03-21 18:13:45 +0100

    Proceso entero automatizado ( add, commit, pull & push )

--- modulos/procesosHijo.js
+++ modulos/procesosHijo.js
@@ -1,38 +1,39 @@
 const { exec } = require('child_process')
 
-async function processAll () {
-    await exec('git add .', (error, stdout, stderr) => {
-        if (error) {
-            console.error(error)
-            return false
-        }
-        console.log(stdout)
-    })
+// async function processAll () {
 
-    await exec('git commit m"Proceso automatizado ( add & commit)"', (error, stdout, stderr) => {
-        if (error) {
-            console.error(error)
-            return false
-        }
-        console.log(stdout)
-    })
+// }
 
-}
+// processAll()
 
-processAll()
+exec('git add .', (error, stdout, stderr) => {
+    if (error) {
+        console.error(error)
+        return false
+    }
+    console.log(stdout)
+})
 
-// exec('git pull origin master', (error, stdout, stderr) => {
-//     if (error) {
-//         console.error(error)
-//         return false
-//     }
-//     console.log(stdout)
-// })
+exec('git commit -m"Proceso entero automatizado ( add, commit, pull & push )"', (error, stdout, stderr) => {
+    if (error) {
+        console.error(error)
+        return false
+    }
+    console.log(stdout)
+})
 
-// exec('git push origin master', (error, stdout, stderr) => {
-//     if (error) {
-//         console.error(error)
-//         return false
-//     }
-//     console.log(stdout)
-// })
+exec('git pull origin master', (error, stdout, stderr) => {
+    if (error) {
+        console.error(error)
+        return false
+    }
+    console.log(stdout)
+})
+
+exec('git push origin master', (error, stdout, stderr) => {
+    if (error) {
+        console.error(error)
+        return false
+    }
+    console.log(stdout)
+})

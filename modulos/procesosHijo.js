40b03222676fb2f8baa24cb8bc0ea45b5aaad0c5
Toni B.Lopez local <tonibassols@outlook.es>
2022-03-21 18:10:42 +0100

    Proceso automatizado ( add & commit)

--- modulos/procesosHijo.js
+++ modulos/procesosHijo.js
@@ -1,9 +1,38 @@
 const { exec } = require('child_process')
 
-exec('git add .', (error, stdout, stderr) => {
-    if (error) {
-        console.error(error)
-        return false
-    }
-    console.log(stdout)
-})
+async function processAll () {
+    await exec('git add .', (error, stdout, stderr) => {
+        if (error) {
+            console.error(error)
+            return false
+        }
+        console.log(stdout)
+    })
+
+    await exec('git commit m"Proceso automatizado ( add & commit)"', (error, stdout, stderr) => {
+        if (error) {
+            console.error(error)
+            return false
+        }
+        console.log(stdout)
+    })
+
+}
+
+processAll()
+
+// exec('git pull origin master', (error, stdout, stderr) => {
+//     if (error) {
+//         console.error(error)
+//         return false
+//     }
+//     console.log(stdout)
+// })
+
+// exec('git push origin master', (error, stdout, stderr) => {
+//     if (error) {
+//         console.error(error)
+//         return false
+//     }
+//     console.log(stdout)
+// })
---
+++ test/ES6.js
@@ -0,0 +1,3 @@
+const { error } = console // esto es igual a esto:  const error = console.error
+
+error('Soy un console.error')
